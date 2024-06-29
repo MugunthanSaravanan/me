da6a8369893ea87215ab3ef1f322de3255379482
Mugunthan Saravanan <sheikriyas532@gmail.com>
2024-06-29 23:33:05 +0530

    Updates

---
+++ precache-manifest.972b932e99bc3708fbe1821d778a16ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a53a3e473a4d47cc4abd183fa01d65",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "77a21473d2d63a8bd8b6",
+    "url": "/me/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4c2e78d819c14af7831f",
+    "url": "/me/static/css/main.7a73787f.chunk.css"
+  },
+  {
+    "revision": "77a21473d2d63a8bd8b6",
+    "url": "/me/static/js/2.5a997e22.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/me/static/js/2.5a997e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2e78d819c14af7831f",
+    "url": "/me/static/js/main.d13a893c.chunk.js"
+  },
+  {
+    "revision": "c2281720cdf10989440d",
+    "url": "/me/static/js/runtime-main.517f3335.js"
+  }
+]);
